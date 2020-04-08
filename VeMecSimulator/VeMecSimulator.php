125fcc8a48c73ccf3ef8e0c3d05acdad45e930da
Lucius <lucianoporta@outlook.com>
2020-04-07 23:48:24 -0300

    rest y websockets

--- VeMecSimulator/VeMecSimulator.php
+++ VeMecSimulator/VeMecSimulator.php
@@ -1,1 +1,27 @@
 <?php
+    require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/include/DataHandler_class.php';
+
+    use Ratchet\Server\IoServer;
+    use Ratchet\Http\HttpServer;
+    use Ratchet\WebSocket\WsServer;
+    use DataHandlerApp\DataHandler;
+
+    $protocol = "websocket";
+
+    if($protocol = "websocket") {
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new DataHandler()
+                )
+            ),
+            8080
+        );
+        $server->run();
+    } else {
+        $dataHandler = new DataHandler();
+    }
+
+    
+
