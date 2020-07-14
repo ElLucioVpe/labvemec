9daa7752f6fddc494704c0829ee964d1f27287ed
Lucius <lucianoporta@outlook.com>
2020-07-13 22:54:18 -0300

    .

--- Parte 2/VeMecSimulator/server.js
+++ Parte 2/VeMecSimulator/server.js
@@ -1,34 +1,32 @@
-const express = require('express');
-const socket = require('socket.io');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const socket = require("socket.io");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: "*" }));
 app.use(bodyParser);
 let x = true;
 
-const server = app.listen(4000,() => {
-    console.log('Started in 4000');
+const server = app.listen(4000, () => {
+  console.log("Started in 4000");
 });
 
 const io = socket(server);
 
-io.sockets.on('connection', (socket) => {
-    console.log(`new connection id: ${socket.id}`);
-    //sendData(socket);
+io.sockets.on("connection", (socket) => {
+  console.log(`new connection id: ${socket.id}`);
+  //sendData(socket);
 
-    socket.on ('envio_datosVeMec', function (msg) {
-        console.log('data: '+msg);
-        var json = JSON.parse(msg);
-        io.sockets.emit('datosVeMec'+json.Id, json);
-    });
+  socket.on("envio_datosVeMec", function (msg) {
+    console.log("data: " + msg);
+    var json = JSON.parse(msg);
+    io.sockets.emit("datosVeMec" + json.id_Slave, json);
+  });
+});
 
-})
-
-function sendData(socket){
-
-    setTimeout(() => {
-        sendData(socket);
-    }, 10000);
+function sendData(socket) {
+  setTimeout(() => {
+    sendData(socket);
+  }, 10000);
 }
